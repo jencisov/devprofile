394e67c625fd9e0265ce1f4126922917a39158e0
Jorge Enciso <jorgeenciso@Jorges-MacBook-Pro.local>
2017-10-29 23:06:36 -0500

    first commit with main storyboard changes

---
+++ devprofile/devprofile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devprofile
+//
+//  Created by Jorge Enciso on 10/29/17.
+//  Copyright © 2017 Jorge Enciso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
